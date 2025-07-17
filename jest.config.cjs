11786d24fbb10b7793cf7b6099803226793f1b63
GamesDotDev <gamesdotdev@outlook.com>
2025-07-17 12:30:09 -0400

    fix: Refine Jest configuration to explicitly include test roots

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,26 +1,9 @@
 module.exports = {
+  roots: [
+    "<rootDir>/__tests__"
+  ],
   testMatch: [
     "**/__tests__/**/*.test.js"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/e2e/"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/e2e/"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/e2e/"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/e2e/"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/e2e/"
   ],
   transform: {
     "^.+\.js$": "babel-jest"
