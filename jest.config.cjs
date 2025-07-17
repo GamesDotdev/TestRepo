1f069dcb1522b9c5184237a5c0af29824775b761
GamesDotDev <gamesdotdev@outlook.com>
2025-07-17 12:34:44 -0400

    fix: Use testPathIgnorePatterns to exclude e2e tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
   testMatch: [
     "**/__tests__/**/*.test.js"
   ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/e2e/"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/e2e/"
+  ],
   transform: {
     "^.+\.js$": "babel-jest"
   },
